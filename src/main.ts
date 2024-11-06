93e8b47e5418fee5cc6debb598c1779a161de770
14N-W4774c3 <iawallac@ucsc.edu>
2024-11-06 01:27:25 -0800

    Test Commit - Button functionality confirmed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+const testButton = document.createElement("button");
+testButton.textContent = "Test";
+document.body.appendChild(testButton);
+testButton.addEventListener("click", () => {
+  console.log("Test");
+  alert("You clicked the button!");
+});
